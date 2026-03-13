5e122d4b2fc3d56d1b2f392a8ac6e6e096773993
rachele <rachelerosal69@gmail.com>
2026-03-13 09:40:16 +0800

    di pa tapus i2

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import LandingPage from "./Pages/LandingPage";
+import CommunityFeed from "./Pages/CommunityFeed";
 import TruthLensWireframes from "./Pages/wireframe/TruthLens_Wireframes";
 
 function App() {
    return (
       <>
-         { <LandingPage /> }
+        { /* LandingPage /> */ }
+          < CommunityFeed />  
          
       </>
    );
