06cc3619380902caa80e5faa54a0f2668ed7ddeb
RenzKirby <117254869+renzkirby@users.noreply.github.com>
2026-03-15 20:58:02 +0800

    add default status for thread model

--- backend/truthlens_backend/api/models.py
+++ backend/truthlens_backend/api/models.py
@@ -58,7 +58,7 @@ class Thread(models.Model):
         "auth.User", on_delete=models.CASCADE, related_name="authored_threads"
     )
     caption = models.TextField(blank=True, null=True)
-    status = models.CharField(max_length=20, blank=True, null=True)
+    status = models.CharField(max_length=20, blank=True, null=True, default="OPEN")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
