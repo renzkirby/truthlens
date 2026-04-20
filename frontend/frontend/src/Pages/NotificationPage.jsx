d6c11a3fbfb1f3a66225715bcca7ad3bc49f8147
RenzKirby <117254869+renzkirby@users.noreply.github.com>
2026-04-20 18:25:20 +0800

    feat: add NotificationPage and SettingsPage components with corresponding styles

---
+++ frontend/frontend/src/Pages/NotificationPage.jsx
@@ -0,0 +1,15 @@
+import NavigationBar from "../components/NavigationBar";
+import "./NotificationPage.css";
+
+function NotificationPage() {
+   return (
+      <>
+         <NavigationBar />
+         <div className="notification-container">
+            <h1>Coming soon...</h1>
+         </div>
+      </>
+   );
+}
+
+export default NotificationPage;
---
+++ frontend/frontend/src/Pages/SettingsPage.jsx
@@ -0,0 +1,15 @@
+import NavigationBar from "../components/NavigationBar";
+import "./SettingsPage.css";
+
+function SettingsPage() {
+   return (
+      <>
+         <NavigationBar />
+         <div className="settings-container">
+            <h1>Coming soon...</h1>
+         </div>
+      </>
+   );
+}
+
+export default SettingsPage;
