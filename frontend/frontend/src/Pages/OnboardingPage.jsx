1c2218c62ff755b9dead05ef543998da7c81c408
RenzKirby <117254869+renzkirby@users.noreply.github.com>
2026-08-22 22:51:01 +0800

    Refactor import statement for useAuth hook

--- frontend/frontend/src/Pages/OnboardingPage.jsx
+++ frontend/frontend/src/Pages/OnboardingPage.jsx
@@ -20,7 +20,7 @@
 
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../hooks/useAuth";
 import Icons from "../components/Icons.jsx";
 import "./OnboardingPage.css";
 
